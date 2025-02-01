ba7a93c3d924c6a5c55554a929373a88a6c2cce7
groshiniprasad <roshiniprasad10@gmail.com>
2025-02-01 16:44:14 +0200

    Refactoring

--- src/components/Home/PrcoessManagement/ProcessManagemnet.jsx
+++ src/components/Home/PrcoessManagement/ProcessManagemnet.jsx
@@ -1,6 +1,8 @@
 import AddPrcessManagement from "../GenericFormModal";
  import { ProcessProvider } from "../../../context/processStageContext";
- import  ProcessManagementTable from "./Table/TableComponent";
+// import  ProcessManagementTable from "./Table/TableComponent"; Doing the code cleanign and refactoring
+ import  ProcessManagementTable from "./PrcoessManagementTable";
+
 
 const ProcessManagement = () => {
   return (
